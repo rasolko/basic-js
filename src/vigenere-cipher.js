bcf028b4d03fe3bee1312895f2c48e082e988ae0
rasolko <vano2029492@mail.ru>
2020-11-01 21:42:34 +0300

    feat: complete vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,54 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isRevers = true){
+    this.isRevers = isRevers;
+  }
+  encrypt(str, key) {
+    let result = [];
+    let strArr = [];
+    for(let i = 0; i < str.length; i++){
+      strArr.push(str.toUpperCase().charCodeAt(i));
+    }
+    let keyArr = [];
+    for(let i = 0; i < str.length; i++){
+      keyArr.push(key.toUpperCase().charCodeAt(i % key.length));
+    }
+    let spaceCount = 0;
+    strArr.map( (item, index) => {
+      if(item >= 65 && item <= 90){
+        result.push(String.fromCharCode((((item - 65) + (keyArr[index - spaceCount] - 65)) % 26) + 65));
+      } else {
+        result.push(String.fromCharCode(item));
+        spaceCount++;
+      }
+    })
+    return this.isRevers ? result.join('') : result.reverse().join('');
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(str, key) {
+    let result = [];
+    let strArr = [];
+    for(let i = 0; i < str.length; i++){
+      strArr.push(str.charCodeAt(i));
+    }
+    let keyArr = [];
+    for(let i = 0; i < str.length; i++){
+      keyArr.push(key.toUpperCase().charCodeAt(i % key.length));
+    }
+    let spaceCount = 0;
+    strArr.map( (item, index) => {
+      if(item >= 65 && item <= 90){
+        if((item - 65) - (keyArr[index - spaceCount] - 65) >= 0){
+          result.push(String.fromCharCode(((item - 65) - (keyArr[index - spaceCount] - 65)) + 65));
+        } else {
+          result.push(String.fromCharCode(( 26 - Math.abs((item - 65) - (keyArr[index - spaceCount] - 65))) + 65));
+        }
+      } else {
+        result.push(String.fromCharCode(item));
+        spaceCount++;
+      }
+    })
+    return this.isRevers ? result.join('') : result.reverse().join('');
   }
 }
 
